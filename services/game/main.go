20d454985bb9de434fa844c1057bde9851444183
Mike Manney <34942709+mjmanney@users.noreply.github.com>
2025-08-17 18:51:22 -0400

    Add basic blackjack game structures and state machine

--- services/game/main.go
+++ services/game/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
-	"casino/libs/fsm"
+	"casino/libs/store"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("game up", fsm.State("Created"))
+	st := store.NewEventStore()
+	g := NewGame(st)
+	fmt.Println("game up", g.State)
+
+	g.StartRound()
+	g.PrintHands()
+
+	g.Hit()
+	g.PrintHands()
+
+	g.Stand()
+	g.DealerPlay()
+	g.Settle()
+
+	fmt.Println("round complete", g.State)
+	fmt.Println("event log:", st.All())
 }
